e357e712f7a516e8263045e813c19eafcdcb75a9
srinija684 <75112894+srinijag684@users.noreply.github.com>
2022-09-07 16:33:08 +0800

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return(<div className='App'>
-    <h1>Weather App</h1>
-  This project is created by Srinija and is {" "}
-  <a
-    href='https://github.com/srinijag684/react-weather-app'
-    target="_blank"
-    rel="noreferrer"
-  >
-    open-sourced on Github
-  </a>
+  return(
+  <div className='App'>
+    <div className='container'>
+      <h1>Weather App</h1>
+      <div className='footer'>
+        This project is created by Srinija and is {" "}
+        <a
+          href='https://github.com/srinijag684/react-weather-app'
+          target="_blank"
+          rel="noreferrer"
+        >
+        open-sourced on Github
+        </a>
+      </div>
+    </div>
  </div>);
     
   
